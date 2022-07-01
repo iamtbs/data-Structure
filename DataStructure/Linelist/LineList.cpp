6c3c01fe60e2d1954fd04673ede85a40a5fccda7
heartlesscat <1367115303@qq.com>
2022-07-01 15:53:34 +0800

    20220701

--- DataStructure/Linelist/LineList.cpp
+++ DataStructure/Linelist/LineList.cpp
@@ -2,24 +2,33 @@
 #include <stdio.h>
 int main() {
 	CLineList cLineList;
-	cLineList.addElem(3);
-	cLineList.addElem(2);
-	cLineList.addElem(1);
-	cLineList.addElem(4);
-	cLineList.addElem(5);
-	cLineList.addElem(6);
-	cLineList.addElem(7);
-	int iR=0;
-	int iS = 0;
-	for (int i = 0; i < 20; i++) {
-		iS = cLineList.getElem(i, &iR);
-		if (iS == STATUS_OK) {
-			printf("STATUS_OK Index:%d ELem:%d \n",i,iS);
-		}
-		else {
-			printf("STATUS_ERROR Index:%d ELem:%d \n", i,iS);
-		}
-	}
+	//cLineList.addElem(3);
+	//cLineList.addElem(2);
+	//cLineList.addElem(1);
+	//cLineList.addElem(4);
+	//cLineList.addElem(5);
+	//cLineList.addElem(6);
+	//cLineList.addElem(7);
+	//int iR=0;
+	//int iS = 0;
+	//for (int i = 0; i < 20; i++) {
+	//	iS = cLineList.getElem(i, &iR);
+	//	if (iS == STATUS_OK) {
+	//		printf("STATUS_OK Index:%d ELem:%d \n",i, iR);
+	//	}
+	//	else {
+	//		printf("STATUS_ERROR Index:%d ELem:%d \n", i, iR);
+	//	}
+	//}
+	
+	//随机存取特性实现
+	//数组地址计算方法
+	// a=数组地址 c=元素变量大小
+	//Loc(a+i)=Loc(a)+i*c;
+	int iSize = sizeof(Elemtype);
+	printf("数组地址：%d 数组第一个元素地址：%d \n", (int)(&cLineList.$data), (int)(&cLineList.$data[0]));
+	printf("数组第五个元素地址：%d 数组第五个元素地址(用数组地址计算)：%d \n", (int)(&cLineList.$data[5]), (int)(&cLineList.$data[0]) + iSize * 5);
+
 	return 1;
 }
 status CLineList::clear() {
@@ -43,6 +52,11 @@ status CLineList::insertElem(int iIndex, Elemtype iEle) {
 		) {
 		return STATUS_ERROR;
 	}
+	if (iIndex + 1 < this->iLength) {//过滤最后一位
+		for (int i = iIndex; i < this->iLength; i++) {
+			this->$data[i + 1] = this->$data[i];
+		}
+	}
 	this->$data[iIndex] = iEle;
 	this->iLength++;
 	return STATUS_OK;
@@ -58,13 +72,9 @@ status CLineList::deleteElem(int iIndex, Elemtype* lpEle) {
 	if (this->iLength <= 0 || iIndex >= this->iLength) {
 		return STATUS_ERROR;
 	}
-	for (int i = this->iLength - 1;i >= iIndex; i--)
-	{
+	for (int i = this->iLength - 1;i > iIndex; i--)
 		this->$data[i-1] = this->$data[i];
-	}
-	//for (int i = iIndex; i <  this->iLength ; i++)
-	//{
-	//	this->$data[i] = this->$data[i+1];
-	//}
+	for (int i = iIndex; i <  this->iLength ; i++)
+		this->$data[i] = this->$data[i+1];
 	this->iLength--;
 }
--- DataStructure/Linelist/LineList.h
+++ DataStructure/Linelist/LineList.h
@@ -5,16 +5,20 @@
 typedef int Elemtype;
 typedef int status;
 //线性表
-//顺序存储结构
+//特点：
+//①仅有一个开始结点和结束结点
+//②最多只有一个前驱结点和一个后继结点。
+//线性表&栈&队列&串
+//顺序存储结构-实现
 class CLineList {
-private:
+public:
 	Elemtype $data[MAXSIZE];
 	int iLength=0;
 public:
-	status getElem(int iIndex,Elemtype*lpEle);
-	status insertElem(int iIndex,Elemtype iEle);
-	status addElem(Elemtype iEle);
-	status deleteElem(int iIndex, Elemtype*lpEle);
-	status clear();
-	int getLength();
+	status insertElem(int iIndex,Elemtype iEle);//插入元素
+	status addElem(Elemtype iEle);//添加元素
+	status deleteElem(int iIndex, Elemtype*lpEle);//删除元素
+	status getElem(int iIndex, Elemtype* lpEle);//获得元素
+	status clear();//清空元素
+	int getLength();//得到元素数目
 };
